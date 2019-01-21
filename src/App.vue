fa48b6fc92b5340e2a1ae2deb85ea5b20f4081aa
HSIKE <893008093@qq.com>
2019-01-21 14:01:43 +0800

    2019/01/21 14:00

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+  @import "assets/css/common.css";
+  @import "assets/css/wheel.css";
+  #app{
+    width:100%;
+  }
+  #app>div{
+    width:100%;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='home'>
+    <div class='header'>
+      <div class='container'>
+        <div class='row nav-bar'>
+          <a class='logo' href=''>
+            <img src='../../static/imgs/logo.jpg' alt='logo'/>
+          </a>
+          <div class='nav'>
+            <a href='../../static/pages/login.html'>登录</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"Home"
+  }
+</script>
+
+<style scoped>
+
+</style>
